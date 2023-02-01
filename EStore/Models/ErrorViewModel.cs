29c3e9c06a2157a812d34ef3411d8d031592adf7
vijay <vijaysharma2306@gmail.com>
2023-02-01 15:16:57 +0530

    file view cahnges

--- EStore/Models/ErrorViewModel.cs
+++ EStore/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace EStore.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
+	<h2>hello git</h2>
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
